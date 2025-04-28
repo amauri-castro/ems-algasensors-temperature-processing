b6d74ee999b87bb57910d348021fc54ac98fd28d
amauri-castro <amauri.p.castro@gmail.com>
2025-04-28 16:26:33 -0300

    Novos arquivos adicionados ao projeto

---
+++ src/test/java/com/amauri/algasensors/temperature/processing/TemperatureProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amauri.algasensors.temperature.processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
